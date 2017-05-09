0e33e5897bc64c795e386c8a43c49ca52a1b755e
MainShayne233 <shaynetremblay@hotmail.com>
2017-05-09 03:18:40 -0400

    uses extra assets

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react'
 import { render } from 'react-dom'
 import App from './ui/app'
 
+function requireAll(r) { r.keys().forEach(r);  }
+requireAll(require.context('../../extra_assets/', true, /[\.js$|\.css$]/));
+
 const root = document.getElementById('root')
 
 render(<App />, root)
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -59,6 +59,13 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
+      },
+      {
         test: /\.scss$/,
         use: [
           'style-loader',
